3a074da8590e50f6151d4c57f1ecd97c907fd3d0
Xiang Zhang <xiang.zhang@essexlg.com>
2017-12-06 18:04:39 +0800

    setup infrastructure.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import DashboardGrid from './DashboardGrid';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header></header>
+        <div className="wrapper">
+            <DashboardGrid/>
+        </div>
       </div>
     );
   }
---
+++ src/DashboardGrid.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import GridLayout from './grid/GridLayout';
+
+class DashboardGrid extends Component {
+    render() {
+        return (
+            <div className="flex layout-centered">
+                <GridLayout >
+
+                </GridLayout>
+            </div>
+        );
+    }
+}
+
+export default DashboardGrid;
